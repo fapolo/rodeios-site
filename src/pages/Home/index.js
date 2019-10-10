8982e5d5854c91a3bbb4ba324de48c0cecc13e5f
Fabricio Polo <fabricio.polo@gmail.com>
2019-10-10 19:47:26 -0300

    add homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import Card from "../../components/Card";
+import api from "../../config/api";
+
+import "./styles.css";
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  const [rodeios, setRodeios] = useState([]);
+
+  async function loadRodeios() {
+    const response = await api.get("/rodeios");
+    setRodeios(response.data);
+  }
+
+  useEffect(() => {
+    loadRodeios();
+  }, [])
+
+  return (
+    <div className="home-container">
+      <div className="header-container">
+        <p><strong>Últimos resultados adicionados</strong></p>
+        <Link to="/">Ver todos >>></Link>
+      </div>
+      <div className="rodeios-container">
+        {rodeios.map(rodeio => <Card props={rodeio} key={rodeio._id} />)}
+        {rodeios.map(rodeio => <Card props={rodeio} key={rodeio._id} />)}
+      </div>
+    </div>
+  );
 }
