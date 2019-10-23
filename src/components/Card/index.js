d790720e2e9a7b2ae34ad0cb6ce85837ef6ebbdc
Fabricio Polo <fabricio.polo@gmail.com>
2019-10-23 20:05:39 -0300

    melhora exibicao data rodeio

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,13 +5,27 @@ import "./styles.css";
 export default function Card({ props }) {
   const { nome, entidade, resultado } = props;
   const ano = new Date(props.data).getFullYear();
+  const dia = new Date(props.data).getDate();
+  let mes = new Date(props.data).getMonth();
+  if (mes === 1) mes = "Janeiro";
+  if (mes === 2) mes = "Fevereiro";
+  if (mes === 3) mes = "Março";
+  if (mes === 4) mes = "Abril";
+  if (mes === 5) mes = "Maio";
+  if (mes === 6) mes = "Junho";
+  if (mes === 7) mes = "Julho";
+  if (mes === 8) mes = "Agosto";
+  if (mes === 9) mes = "Setembro";
+  if (mes === 10) mes = "Outubro";
+  if (mes === 11) mes = "Novembro";
+  if (mes === 12) mes = "Dezembro";
 
   return (
     <div className='card-container'>
       <h1>{nome}</h1>
       <div className='card-textarea'>
         <p>
-          <strong>Data</strong>: {ano}
+          <strong>Data</strong>: {dia} de {mes} de {ano}
         </p>
         <p>
           <strong>Organizador</strong>: {entidade.nome}
